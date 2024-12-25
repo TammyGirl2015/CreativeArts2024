d1fd15a06f7d03ec35f994ee78319f3cd36b1dcf
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-12-25 10:33:40 +0000

    add to shop views file

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='shop_home'),
     path('checkout/', views.checkout, name='checkout'),
+    path('login/', auth_views.LoginView.as_view(template_name='shop/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,13 @@
 import stripe
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+# Checkout View
 def checkout(request):
     if request.method == "POST":
         try:
@@ -19,5 +22,19 @@ def checkout(request):
             return JsonResponse({'error': str(e)}, status=400)
     return render(request, 'shop/checkout.html')
 
+# Index HTML View
 def index(request):
     return render(request, 'shop/index.html')  
+
+# Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'shop/register.html', {'form': form})
