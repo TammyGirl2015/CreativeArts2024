b0074378e1332ed64365249457b76ef6da9eb453
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-12-27 09:32:25 +0000

    edit html pages

--- services/urls.py
+++ services/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.services_view, name='services'),
     path('design-service/', views.design_service_view, name='design_service'),
+    path('logo-design/', views.logo_design_view, name='logo_design'),
+    path('poster-design/', views.poster_design_view, name='poster_design'),
+    path('social-media-design/', views.social_media_design_view, name='social_media_design'),
 ]
--- services/views.py
+++ services/views.py
@@ -6,3 +6,11 @@ def services_view(request):
 def design_service_view(request):
     return render(request, 'services/design_service.html')
 
+def logo_design_view(request):
+    return render(request, 'services/logo_design.html')
+
+def poster_design_view(request):
+    return render(request, 'services/poster_design.html')
+
+def social_media_design_view(request):
+    return render(request, 'services/social_media_design.html')
