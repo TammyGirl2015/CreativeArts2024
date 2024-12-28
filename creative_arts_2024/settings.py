64782b03ef0454baf12bc98250b1b2c8f46f0bed
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-12-28 11:42:30 +0000

    add postgresql for heroku

--- creative_arts_2024/settings.py
+++ creative_arts_2024/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +79,9 @@ WSGI_APPLICATION = 'creative_arts_2024.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    )
 }
 
 # Stripe keys
